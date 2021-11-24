1b1b6de1dc6315ab949669cdbf90f807a3f66782
dimassulz <dimassulz@gmail.com>
2021-11-24 20:21:06 -0300

    array busca filtro usando o DOM

--- javascript/arrays_busca_filtro.js
+++ javascript/arrays_busca_filtro.js
@@ -22,11 +22,11 @@ console.log(senior)
 
 //array de usuarios
 var usuarios = [
-    {nome: "Irineu", login:"irineu1234@gmail.com", senha:"@12456"},
-    {nome: "Marta", login:"martafeliz@outlook.com", senha:"@1244356"},
-    {nome: "Vera", login:"vera_234@hotmail.com", senha:"@43434"},
-    {nome: "Francisco", login:"chiquinho_1234@gmail.com", senha:"@43434"},
-    {nome: "Antonio", login:"antonio_321654@gmail.com", senha:"@65656"}
+    {id: 1, nome: "Irineu", login:"irineu1234@gmail.com", senha:"@12456"},
+    {id: 2, nome: "Marta", login:"martafeliz@outlook.com", senha:"@1244356"},
+    {id: 3, nome: "Vera", login:"vera_234@hotmail.com", senha:"@43434"},
+    {id: 4, nome: "Francisco", login:"chiquinho_1234@gmail.com", senha:"@43434"},
+    {id: 5, nome: "Antonio", login:"antonio_321654@gmail.com", senha:"@65656"}
 ]
 
 //filtro se quiser filtrar o array usuario pela senha @12456
@@ -63,5 +63,82 @@ const encontrarNome = function(){
         alert(`Nome: ${encontrado[0].nome} \nLogin: ${encontrado[0].login} \nSenha: ${encontrado[0].senha}`)
     }
 }
+
+/**
+ * Encontrar um login ou senha ou nome no array de usuarios
+ */
+ const filtrarDadosUsuario = function(){
+    //verifica se o valor nao esta preenchido
+    if(valor.value === ""){
+        //listar todos os usuario na TR
+        mostrarTodosUsuarioTr()
+    }   
+    //filtrar os usuario por login ou nome ou senha de acordo com o texto digitado no campo
+    let arrUsuariosEncontrados = usuarios.filter(function(usuario){
+        return  usuario.login.includes(valor.value.toLocaleLowerCase()) 
+                || usuario.nome.includes(valor.value) 
+                || usuario.senha.includes(valor.value.toLocaleLowerCase()) 
+    })
+    //se o array encontrado tiver tamanho 0 o sistema uma tr com registro nao encontrado
+    if(arrUsuariosEncontrados.length === 0){
+        //tbody id=dadosUsuario e insere o html com a informação abaixo
+        dadosUsuario.innerHTML = `
+        <tr>
+            <td colspan="4"> - NENHUM REGISTRO ENCONTRADO -  </td>
+        </tr>
+        `
+    }else{
+        //mostrar os dados filtrados na TR
+        mostrarUsuarioFiltradoTr(arrUsuariosEncontrados)
+    }
+ }
+
+ /**
+  * Recebe como parametro o array filtrado
+  * @param {array} dadoFiltrado 
+  */
+const mostrarUsuarioFiltradoTr = function(dadoFiltrado){
+    let mostrarTrTela = ""
+    //iterar o array filtrado montando um HTML de tr e td com os dados filtrados
+    dadoFiltrado.forEach(function(usuario){
+        mostrarTrTela += `
+        <tr>
+            <td>${usuario.id}</td>
+            <td>${usuario.nome}</td>
+            <td>${usuario.login}</td>
+            <td>${usuario.senha}</td>
+        </tr>
+        `
+    })
+    //tbody id=dadosUsuario e insere o html montado acima
+    dadosUsuario.innerHTML = mostrarTrTela
+}
+
+/**
+ * Lista todos os usuarios do array usuarios 
+ */
+const mostrarTodosUsuarioTr = function(){
+    let mostrarTrTela = ""
+    //iterar o array usuarios montando um HTML de tr e td com os todos os dados
+    usuarios.forEach(function(usuario){
+        mostrarTrTela += `
+        <tr>
+            <td>${usuario.id}</td>
+            <td>${usuario.nome}</td>
+            <td>${usuario.login}</td>
+            <td>${usuario.senha}</td>
+        </tr>
+        `
+    })
+    //tbody id=dadosUsuario e insere o html montado acima
+    dadosUsuario.innerHTML = mostrarTrTela
+}
+
 //pegar o valor do campo na tela quando for retirado o foco do campo
-valor.onblur = encontrarNome
+//valor.onblur = filtrarDadosUsuario
+
+//poderia usar um evento onkeyup para fazer quando o usuario estiver digitando
+valor.onkeyup = filtrarDadosUsuario
+
+//iniciando os dados da tabela
+mostrarTodosUsuarioTr()
