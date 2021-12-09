b36c8d4ef01e8e5119d66fef6e0a23b8a0eab7a2
dimassulz <dimassulz@gmail.com>
2021-12-09 19:52:11 -0300

    atualizando o fetch

--- javascript/fetch/fetch.js
+++ javascript/fetch/fetch.js
@@ -1,21 +1,19 @@
-
-let url = "./estados-cidades.json"
-fetch(url)
+fetch("./estados-cidades.json")
     .then((resp) => resp.json())
     .then(function (data) {
         let estados = data.estados;
         let options = ""
         estados.forEach(e => options += `<option value="${e.sigla}">${e.nome}</option>`)
         listaEstados.innerHTML = options
+        
         listaEstados.addEventListener('change', function () {
-            let optionsMun = ""
             let municipios = []
+            let optionsMun = ""
             municipios = estados.filter(e => e.sigla === this.value)[0].cidades
-            municipios.forEach(v => optionsMun += `<option value="${v}">${v}</option>`)
+            municipios.forEach(nomeMunicipio => optionsMun += `<option value="${nomeMunicipio}">${nomeMunicipio}</option>`)
             listaMun.innerHTML = optionsMun
         });
-        
     })
     .catch(function (error) {
-        console.log(error);
-    });
+        console.log(error)
+    })
